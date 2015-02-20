0c9388287adb7198a290fa58a0b960bf18247dc4
developerdizzle <david.bohner@gmail.com>
2015-02-20 03:28:46 +0000

    Updates javascript to automatically add classes; adds defaults; renames .push to .navbar-push

--- js/push-menu.js
+++ js/push-menu.js
@@ -1,18 +1,24 @@
 (function($) {
     $(function() {
-        $('[data-toggle="push"]').on('click', function(e) {
+        $('[data-toggle="push"]').each(function() {
             var $this = $(this);
+
+            var $target = $($this.data('target') || $this.attr('href') || '#navbar');
+            var direction = $this.data('direction') || 'left';
             
-            var $target = $($this.data('target') || $this.attr('href'));
-            var direction = $this.data('direction')
-            var $canvas = $($this.data('canvas'));
+            $target.addClass('navbar-push').addClass('navbar-push-' + direction);
             
-            $this.toggleClass('active');
-            if ($this.is('.fa-bars')) {
-                $this.toggleClass('fa-rotate-90');
-            }
-            $canvas.toggleClass('pushed-' + direction);
-            $target.toggleClass('in');
+            var $canvas = $($this.data('canvas') ||  'body');
+            $canvas.addClass('push-canvas');
+            
+            $this.on('click', function(e) {
+                $this.toggleClass('active');
+                if ($this.is('.fa-bars')) {
+                    $this.toggleClass('fa-rotate-90');
+                }
+                $canvas.toggleClass('pushed-' + direction);
+                $target.toggleClass('in');
+            });
         });
     });
 })(jQuery);
