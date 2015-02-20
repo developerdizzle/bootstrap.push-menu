c71dd1c96f6d831f2b7b87e1816ed418d1127ab6
developerdizzle <david.bohner@gmail.com>
2015-02-20 04:03:33 +0000

    Adds bower component for bootstrap.push-menu

---
+++ bower_components/bootstrap.push-menu/dist/js/push-menu.js
@@ -0,0 +1,25 @@
+(function($) {
+    $(function() {
+        $('[data-toggle="push"]').each(function() {
+            var $this = $(this);
+
+            var $target = $($this.data('target') || $this.attr('href') || '#navbar');
+            var direction = $this.data('direction') || 'left';
+            
+            $target.addClass('navbar-push').addClass('navbar-push-' + direction);
+            
+            var $canvas = $($this.data('canvas') ||  'body');
+            $canvas.addClass('push-canvas');
+            
+            $this.on('click', function(e) {
+                $this.toggleClass('active');
+                if ($this.is('.fa-bars')) {
+                    $this.toggleClass('fa-rotate-90');
+                }
+                $canvas.toggleClass('pushed-' + direction);
+                $target.toggleClass('in');
+            });
+        });
+    });
+})(jQuery);
+
